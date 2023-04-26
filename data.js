5b3b0975aaeec2181aa26bcfb4ee1c99c236fe51
youhok <117980159+youhok@users.noreply.github.com>
2023-04-26 15:10:44 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,153 @@
+let data = [
+    [
+        "Part 1",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "61",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/FOPyxX9Ji09NlVV5WmTX.png",
+        "A14",
+        "5",
+        "ERO921",
+        "Central Components"
+    ],
+    [
+        "Part 2",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "617",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/uhGAARXbJR1KhzG0Xrsd.png",
+        "A14",
+        "25",
+        "ERO-CC1",
+        "Peripherals"
+    ],
+    [
+        "Part 3",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "776",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/3xbjeQ37IvN4LYao3eSa/pub/pAyWQ7VySc9j7pgESuO7.jpg",
+        "A15",
+        "5",
+        "ERO-CV1",
+        "Central Components"
+    ],
+    [
+        "Part 4",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "377",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/3xbjeQ37IvN4LYao3eSa/pub/3PHcAXUxsgtVfQyxemzm.jpg",
+        "A16",
+        "25",
+        "ERO-LI1",
+        "Central Components"
+    ],
+    [
+        "Part 5",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "997",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/tuRJ3DEfCg2DgklVCNkD.png",
+        "A17",
+        "25",
+        "ERO-LC1",
+        "Central Components"
+    ],
+    [
+        "Part 6",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "24",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/US9YlhOH7w7jyYVQD6Zg.png",
+        "A18",
+        "25",
+        "ERO-WG1",
+        "Connectors"
+    ],
+    [
+        "Part 7",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "693",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/1hQ7mk4H3lPPb1SJ1BoC.png",
+        "A19",
+        "5",
+        "ERO-CS1",
+        "Central Components"
+    ],
+    [
+        "Part 8",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "263",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/kelMETJvpuPFNN7mOyKn.png",
+        "B2",
+        "5",
+        "SDI-989",
+        "Connectors"
+    ],
+    [
+        "Part 9",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "380",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/XMbJRqy8gauvGPQINhRe.png",
+        "B3",
+        "25",
+        "DSSO-790",
+        "Connectors"
+    ],
+    [
+        "Part 10",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "355",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/3xbjeQ37IvN4LYao3eSa/pub/dID0naBx6n0hH4HRbhZ2.jpg",
+        "B4",
+        "25",
+        "DSF-DD0",
+        "Connectors"
+    ],
+    [
+        "Part 11",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "1043",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/JUFABLkcrpz5h17R8l6Z.png",
+        "B5",
+        "25",
+        "WID-WI9",
+        "Body"
+    ],
+    [
+        "Part 12",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "568",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/3xbjeQ37IvN4LYao3eSa/pub/CjuYwFqtUhczKFumhwkG.jpg",
+        "B6",
+        "25",
+        "WID-W77",
+        "Body"
+    ],
+    [
+        "Part 13",
+        "Neque sodales massa netus facilisis parturient tellus cras eros venenatis cum ad laoreet molestie commodo, eu magna adipiscing senectus suscipit condimentum blandit tristique quisque conubia facilisi risus pharetra. Lacinia nec aenean euismod velit vehicula etiam ultrices dictumst class, feugiat nibh lorem hendrerit eu consectetur lacus lobortis taciti, quam purus erat condimentum tellus quisque vivamus cubilia. Aenean vivamus tempus euismod auctor sapien penatibus magnis, felis sociis pulvinar eleifend diam nisl dignissim, ullamcorper lectus mauris ante montes nulla.",
+        "665",
+        "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/MINA1H5lSx6UQsE9PZ2n/pub/77V1boRzqAKCwQ8o0PNj.png",
+        "B7",
+        "25",
+        "WIBG-098",
+        "Body"
+    ]
+];
+
+let container = document.getElementById("container");
+
+for (let i = 0; i < data.length ; i++) {
+    container.innerHTML +=
+    `
+    <div class="card">
+    <div class="imge">
+        <img src="${data[i][3]}">
+    </div>
+    <h3>${data[i][7]}</h3>
+    <h4>${data[i][0]}</h4>
+    <p>${data[i][6]}</p>
+</div>
+    
+    `  
+     ;
+
+}
+
+
